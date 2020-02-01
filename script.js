f600fd81fb56dc663dc1c5c29221700b1db92d55
LeoFdz <lfernandez79@github.com>
2020-02-01 15:27:00 -0600

    added columns and append to page

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 $(document).ready(function () {
+    
+    // append time to the page and function to update dynamic
     function timeUpdate() {
         $("#currentDay").text(moment().format("dddd, MMMM Do h:mm:ss a"));
     } 
     setInterval(timeUpdate, 1000);
-       
-    
-    // for (let i=8; i<17; i++) {
+          
+    for (let i=8; i<17; i++) {
         // const element = array[i];
 
-        // // Create row
-        // var row = $(`<div data-time=${i} id="${i}" class="row">`);
+        // Create row
+        var row = $(`<div data-time=${i} id="${i}" class="row">`);
 
-        // // Create a column
-        // var column1 = $('<div class="col-sm-2"> <p class="hour">' + formatAMPM(i) + "</p>");
+        // Create a columns
+        var column1 = $('<div class="col-sm-2"> <p class="hour">' + currentTime(i) + "</p>");
+        var column2 = $(`<div class="col-sm-8"><textarea id=text${i} class="col-md-12 description" placeholder="To do item"></textarea>`);
+        var column3 = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`);
+        
+        //append column to row
+        row.append(column1, column2, column3);
+       
+        // add rows to container
+        $(".container").append(row);
 
-        // //append column to row
-        // row.append(column1);
 
-        // // add rows to container
-        // // $(".container").append(row);
+    }
+    
+    function currentTime(event) {
+        var time = event >= 12 ? "pm" : "am";
+        event = event % 12;
+        event = event ? event : 12;
+        return event + time;
+    }
 
 
-    // }
 })
