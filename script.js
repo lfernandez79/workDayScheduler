9cebf17541d2182eb7ec503970f02b2a041a16bd
LeoFdz <lfernandez79@github.com>
2020-02-01 13:26:05 -0600

    Added date and time to age using moment()

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $("#currentDay").text(moment().format("dddd, MMMM Do h:mm a"));
+    // for (let i=8; i<17; i++) {
+        // const element = array[i];
+
+        // // Create row
+        // var row = $(`<div data-time=${i} id="${i}" class="row">`);
+
+        // // Create a column
+        // var column1 = $('<div class="col-sm-2"> <p class="hour">' + formatAMPM(i) + "</p>");
+
+        // //append column to row
+        // row.append(column1);
+
+        // // add rows to container
+        // // $(".container").append(row);
+
+
+    // }
+})
