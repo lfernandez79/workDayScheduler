cd60a82e629824ba7de1f35352adee7c90121980
LeoFdz <lfernandez79@github.com>
2020-02-01 15:53:18 -0600

    added bootstrap

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     } 
     setInterval(timeUpdate, 1000);
           
-    for (let i=8; i<17; i++) {
+    for (let i=8; i<18; i++) {
         // const element = array[i];
 
         // Create row
@@ -15,7 +15,7 @@ $(document).ready(function () {
         // Create a columns
         var column1 = $('<div class="col-sm-2"> <p class="hour">' + currentTime(i) + "</p>");
         var column2 = $(`<div class="col-sm-8"><textarea id=text${i} class="col-md-12 description" placeholder="To do item"></textarea>`);
-        var column3 = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`);
+        var column3 = $(`<div class="col-sm-2"><button class="col-md-12 btn btn-danger saveBtn" id=${i}><i class="fas fa-cloud fa-3x"></i></button>`);
         
         //append column to row
         row.append(column1, column2, column3);
