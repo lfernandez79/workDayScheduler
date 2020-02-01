2db3a07f1bb32276753b961eb5a59519516137f9
LeoFdz <lfernandez79@github.com>
2020-02-01 16:31:43 -0600

    added more style to planner

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ $(document).ready(function () {
         var row = $(`<div data-time=${i} id="${i}" class="row">`);
 
         // Create a columns
-        var column1 = $('<div class="col-sm-2 bg-light mb-4 text-center"> <h3 class="hour">' + currentTime(i) + "</h3>");
-        var column2 = $(`<div class="col-sm-8"><textarea id=text${i} class="col-md-12 bg-light description" placeholder="To do item"></textarea>`);
-        var column3 = $(`<div class="col-sm-2"><button class="col-md-12 btn btn-danger saveBtn" id=${i}><i class="fas fa-cloud fa-3x"></i></button>`);
+        var column1 = $('<div class="col-md-2 bg-light mb-4 text-center text-success"> <h3 class="hour">' + currentTime(i) + "</h3>");
+        var column2 = $(`<div class="col-md-8"><textarea id=text${i} class="col-md-12 bg-light description" placeholder="To do item"></textarea>`);
+        var column3 = $(`<div class="col-md-2"><button class="col-md-12 btn btn-danger saveBtn" id=${i}><i class="fas fa-cloud fa-3x"></i></button>`);
         
         //append column to row
         row.append(column1, column2, column3);
