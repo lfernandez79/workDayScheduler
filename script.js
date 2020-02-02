e94cb050872e1d845093cc6a30b45c611e1a9ecd
LeoFdz <lfernandez79@github.com>
2020-02-02 10:15:49 -0600

    Added font style to textarea and removedd repeated comments

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     
-    // append time to the page and function to update dynamic
+    // append time to the page and function to update dynamically
     function timeUpdate() {
         $("#currentDay").text(moment().format("dddd, MMMM Do h:mm:ss a"));
     } 
     setInterval(timeUpdate, 1000);
           
+    // loop to create rows  from 8 to 5 PM
     for (let i=8; i<18; i++) {
-        // const element = array[i];
 
         // Create row
         var row = $(`<div data-time=${i} id="${i}" class="row">`);
@@ -15,7 +15,7 @@ $(document).ready(function () {
         // Create a columns
         var column1 = $('<div class="col-md-2 bg-light mb-4 text-center text-success"> <h3 class="hour">' + currentTime(i) + "</h3>");
         var column2 = $(`<div class="col-md-8"><textarea id=text${i} class="col-md-12 bg-light description" placeholder="To do item"></textarea>`);
-        var column3 = $(`<div class="col-md-2"><button class="col-md-12 btn btn-danger saveBtn" id=${i}><i class="fas fa-cloud fa-3x"></i></button>`);
+        var column3 = $(`<div class="col-md-2"><button class="col-md-6 btn btn-danger saveBtn" id=${i}><i class="fas fa-cloud fa-lg"></i></button>`);
         
         //append column to row
         row.append(column1, column2, column3);
